cdbeb95e9348354eba11c961fcb69e80cfaa3cfb
ke.xu <ke.xu@beibei.com>
2020-02-12 21:08:12 +0800

    Initial Commit

---
+++ sisp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sisp
+//
+//  Created by 徐可 on 2020/2/12.
+//  Copyright © 2020 Beibei Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
